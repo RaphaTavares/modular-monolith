927adc60f8fb949b7722ff363efae8b42e3bbc27
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-10-08 22:52:21 -0300

    feat: implementa o evento de criar eventos

---
+++ Evently.Modules.Events.Domain/Abstractions/DomainEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace Evently.Modules.Events.Domain.Abstractions;
+
+public abstract class DomainEvent : IDomainEvent
+{
+    protected DomainEvent(Guid id, DateTime occurredOnUtc)
+    {
+        Id = id;
+        OccurredOnUtc = occurredOnUtc;
+    }
+    protected DomainEvent()
+    {
+        Id = Guid.NewGuid();
+        OccurredOnUtc = DateTime.UtcNow;
+    }
+
+
+    public Guid Id { get; init; }
+    public DateTime OccurredOnUtc { get; init; }
+}
