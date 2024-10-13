81cd07b37d39e26aafe30f43e0c72238a55358d0
RaphaTavares <raphael_tavares2000@hotmail.com>
2024-10-12 21:05:23 -0300

    lesson 12 - refactor: implement a better approach to dependencies with common libs

--- Evently.Api/Program.cs
+++ Evently.Api/Program.cs
@@ -1,5 +1,7 @@
 using Evently.Api.Extensions;
 using Evently.Modules.Events.Infrastructure;
+using Evently.Common.Application;
+using Evently.Common.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.CustomSchemaIds(type => type.FullName);
 });
+
+builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
+
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
 
 builder.Services.AddEventsModule(builder.Configuration);
 
